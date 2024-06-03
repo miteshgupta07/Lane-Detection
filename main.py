45cbfa246ee76e745939b045c68e2c6c25e63b51
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-06-03 20:14:18 +0530

    Commit

--- main.py
+++ main.py
@@ -15,9 +15,21 @@ def region_of_interest(image):
     masked_image=cv2.bitwise_and(image,mask)
     return masked_image
 
+def display_lines(image,lines):
+    line_image=np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2=line.reshape(4)
+            cv2.line(line_image,(x1,y1),(x2,y2),(0,255,0),10)
+    return line_image        
+
+
 img=cv2.imread("test_image.jpg")
 
 canny=image_to_canny(img)
 cropped_img=region_of_interest(canny)
-cv2.imshow("Image",cropped_img)
+lines=cv2.HoughLinesP(cropped_img,2,np.pi/180,threshold=100,lines=np.array([]),minLineLength=40,maxLineGap=5)
+line_image=display_lines(img,lines)
+combo_image=cv2.addWeighted(img,0.8,line_image,1,1)
+cv2.imshow("Image",combo_image)
 cv2.waitKey(0)
