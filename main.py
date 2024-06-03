f735be16c7744ca6c4c8e604f920ba81c7d479ab
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-06-03 19:54:23 +0530

    Commit

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import cv2
+import numpy as np
+
+def image_to_canny(image):
+    gray_img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+    blur_img=cv2.GaussianBlur(gray_img,(5,5),0)
+    canny_img=cv2.Canny(blur_img,50,150)
+    return canny_img
+
+def region_of_interest(image):
+    height=image.shape[0]
+    triangle=np.array([[(200,height),(1100,height),(550,250)]])
+    mask=np.zeros_like(image)
+    cv2.fillPoly(mask,triangle,255)
+    masked_image=cv2.bitwise_and(image,mask)
+    return masked_image
 
 img=cv2.imread("test_image.jpg")
 
-gray_img=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-blur_img=cv2.GaussianBlur(gray_img,(5,5),10)
-canny_img=cv2.Canny(blur_img,50,150)
-
-cv2.imshow("Image",canny_img)
+canny=image_to_canny(img)
+cropped_img=region_of_interest(canny)
+cv2.imshow("Image",cropped_img)
 cv2.waitKey(0)
